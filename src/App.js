9bbe05d133403a63a8ec5f33451577eed4025319
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2022-08-30 15:35:43 +0600

    create header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Header } from './components';
 
 const App = () => {
   return (
     <div>
-      <h2 className="w-screen h-screen flex items-center justify-center text-blue-600">hello world</h2>
+      <Header/>
     </div>
   );
 };
