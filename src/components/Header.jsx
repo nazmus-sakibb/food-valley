cca16cf9a25ab52a9781a42e4b34243f188ea91c
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2022-08-30 15:52:58 +0600

    working on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div className="fixed z-50 w-screen bg-slate-300 p-6">
+        <div className="fixed z-50 w-screen bg-slate-300 p-6 px-16">
             header 
         </div>
     );
