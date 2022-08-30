03b36989879c9d70af2d19298c3ab61619d75042
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2022-08-30 15:09:13 +0600

    setup dependencies

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
   return (
     <div>
-      <h2>hello world</h2>
+      <h2 className="w-screen h-screen flex items-center justify-center text-blue-600">hello world</h2>
     </div>
   );
 };
