f2870a6d3ab783aa8adc13a97c5a7f052316854a
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2022-09-07 01:34:22 +0600

    k

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import Logo from '../img/logo.png';
 
 const Header = () => {
     return (
-        <div className="fixed z-50 w-screen bg-slate-300 p-6 px-16">
+        <div className="fixed z-50 w-screen p-6 px-16">
             {/* desktop & tablet */}
-            <div className="hidden md:flex w-full h-full"></div>
+            <div className="hidden md:flex w-full h-full">
+                <div className='flex items-center gap-2'>
+                    <img src={Logo} alt="logo" className='w-10 object-cover' />
+                    <p className="text-headingColor text-xl font-bold">FoodValley</p>
+                </div>
+            </div>
 
 
             {/* Mobile */}
