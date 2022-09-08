3c4c3efefbd2981c14581fde8ddc6a03402f50f8
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2022-09-08 13:47:27 +0600

    navbar setup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,19 +3,28 @@ import Logo from '../img/logo.png';
 
 const Header = () => {
     return (
-        <div className="fixed z-50 w-screen p-6 px-16">
+        <header className="fixed z-50 w-screen p-6 px-16">
             {/* desktop & tablet */}
             <div className="hidden md:flex w-full h-full">
                 <div className='flex items-center gap-2'>
-                    <img src={Logo} alt="logo" className='w-10 object-cover' />
+                    <img src={Logo} alt="logo" className='w-8 object-cover' />
                     <p className="text-headingColor text-xl font-bold">FoodValley</p>
                 </div>
-            </div>
+
+
+                {/* navbar */}
+                <ul>
+                    <li>Home</li>
+                    <li>Menu</li>
+                    <li>About</li>
+                    <li>Service</li>
+                </ul>
+            </div> 
 
 
             {/* Mobile */}
             <div className="flex md:hidden w-full h-full"></div> 
-        </div>
+        </header>
     );
 };
 
