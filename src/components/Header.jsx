5b033a06c3e0b31386ca0b1fbbe000f8d926dcaa
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2022-09-08 23:57:58 +0600

    setup context provider

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,15 +6,22 @@ import { MdShoppingBasket } from 'react-icons/md';
 import { Link } from 'react-router-dom';
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { app } from '../firebase.config';
+import { useStateValue } from '../context/StateProvider';
+import { actionType } from '../context/reducer';
 
 const Header = () => {
 
     const firebaseAuth = getAuth(app);
     const provider = new GoogleAuthProvider();
+
+    const [{user}, dispatch] = useStateValue();
     
     const login = async () => {
-        const response = await signInWithPopup(firebaseAuth, provider);
-        console.log(response); 
+        const {user: {refreshToken, providerData}} = await signInWithPopup(firebaseAuth, provider);
+        dispatch({
+            type: actionType.SET_USER,
+            user: providerData[0]
+        })
     }
 
 
