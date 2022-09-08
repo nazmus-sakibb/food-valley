f6a9c6fc4cf207865fa7619281bafee6eb36cdef
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2022-09-08 14:40:02 +0600

    cart button added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Logo from '../img/logo.png';
+import {MdShoppingBasket} from 'react-icons/md';
 
 const Header = () => {
     return (
+        // Header
         <header className="fixed z-50 w-screen p-6 px-16">
             {/* desktop & tablet */}
-            <div className="hidden md:flex w-full h-full">
+            <div className="hidden md:flex w-full h-full items-center justify-between">
                 <div className='flex items-center gap-2'>
                     <img src={Logo} alt="logo" className='w-8 object-cover' />
                     <p className="text-headingColor text-xl font-bold">FoodValley</p>
@@ -13,17 +15,26 @@ const Header = () => {
 
 
                 {/* navbar */}
-                <ul>
-                    <li>Home</li>
-                    <li>Menu</li>
-                    <li>About</li>
-                    <li>Service</li>
+                <ul className='flex items-center gap-8'>
+                    <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer '>Home</li>
+                    <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer '>Menu</li>
+                    <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer '>About</li>
+                    <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer '>Service</li>
                 </ul>
-            </div> 
+                 
+
+                {/* cart */}
+                <div className='relative flex items-center justify-center'>
+                    <MdShoppingBasket className='text-textColor text-2xl ml-8 cursor-pointer'/>
+                    <div className='absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center'>
+                        <p className='text-xs text-white font-semibold'>2</p>
+                    </div>
+                </div>
+            </div>
 
 
             {/* Mobile */}
-            <div className="flex md:hidden w-full h-full"></div> 
+            <div className="flex md:hidden w-full h-full"></div>
         </header>
     );
 };
