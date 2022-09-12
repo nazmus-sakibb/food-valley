d887ef53f22a7657be84ca7727d682bbda13cd22
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2022-09-12 13:06:13 +0600

    implement animation effect on user profile logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -27,6 +27,8 @@ const Header = () => {
                 user: providerData[0]
             });
             localStorage.setItem('user', JSON.stringify(providerData[0]));
+        } else {
+            setIsMenu(!isMenu);
         }
     };
 
@@ -67,7 +69,7 @@ const Header = () => {
 
                         {
                             isMenu && (
-                                <div className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0">
+                                <motion.div initial={{ opacity: 0, scale: 0.6 }} animate={{ opacity: 1, scale: 1 }} exit={{ opacity: 0, scale: 0.6 }} className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0">
                                     {
                                         user && user.email === 'nazmus.sakibbb1@gmail.com' && (
                                             <Link to={"/createItem"}>
@@ -76,7 +78,7 @@ const Header = () => {
                                         )
                                     }
                                     <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'>Logout <MdLogout /></p>
-                                </div>
+                                </motion.div>
                             )
                         }
                     </div>
